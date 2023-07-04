8bc4fbc54bbbb1d301a918bf6f599d9d0e652044
jramirez <ramirezjose473@gmail.com>
2023-07-03 21:20:17 -0300

    fechas de inicio y fin

--- Front/assets/js/script.js
+++ Front/assets/js/script.js
@@ -1,4 +1,16 @@
 var token;
+var inicio;
+var fin;
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var mmAtras = String(today.getMonth()).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+
+inicio = dd+mm+yyyy;
+fin=dd+mmAtras+yyyy; 
 
 
 window.addEventListener('load', function() {
@@ -134,9 +146,9 @@ function cargar(){
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
     let urlencoded = new URLSearchParams();
-    urlencoded.append("id", "1");
-    urlencoded.append("fechaInicio", "01012023");
-    urlencoded.append("fechaFin", "01062023");
+    urlencoded.append("id", "1"); //DOLAR
+    urlencoded.append("fechaInicio", inicio);
+    urlencoded.append("fechaFin", fin);
 
     fetch("https://dolarsi.com/adm/api/estadisticas/?type=getHistorico", {
         method: 'POST',
@@ -147,7 +159,6 @@ function cargar(){
     .then(result => {
         for (var datos of result) 
         {
-            console.log(datos);
             uploadMoneda(datos);
         }
     })
